67f365fdd6e84cf450939a2edc1fda9347a65711
Daniel Gililov <156680768+FunnyShaker@users.noreply.github.com>
2026-09-14 11:05:05 -0400

    Add UserProfile route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import Login from "./pages/login"
-import Signup from "./pages/signUp"
-import Home from "./pages/home"
-import ViewFlights from "./pages/viewFlights"
-import Cart from "./pages/cart"
-
-function App() {
-  return (
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/flights" element={<ViewFlights />} />
-        <Route path="/cart" element={<Cart />} />
-
-      </Routes>
-
-    </BrowserRouter>
-  )
-}
-
+import { BrowserRouter, Routes, Route } from "react-router-dom" 
+ 
+import Login from "./pages/login" 
+import Signup from "./pages/signUp" 
+import Home from "./pages/home" 
+import ViewFlights from "./pages/viewFlights" 
+import Cart from "./pages/cart" 
+import UserProfile from "./pages/userProfile"
+ 
+function App() { 
+  return ( 
+    <BrowserRouter> 
+ 
+      <Routes> 
+ 
+        <Route path="/" element={<Login />} /> 
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/flights" element={<ViewFlights />} /> 
+        <Route path="/cart" element={<Cart />} /> 
+        <Route path="/profile" element={<UserProfile />} />
+ 
+      </Routes> 
+ 
+    </BrowserRouter> 
+  ) 
+} 
+ 
 export default App
